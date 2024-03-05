868e9381698f9910cf669eedba5bfb17afda9925
djkeu <mrkooij@gmail.com>
2024-03-05 18:55:21 +0100

    Fix youtube test

--- domain_from_url.py
+++ domain_from_url.py
@@ -1,7 +1,7 @@
 import re
 
 def domain_name(url):
-    url = re.sub("([A-Za-z]*://)?(www*.)?(\\.[A-Za-z]*)?(/[A-Za-z]*)?", "", url)
+    url = re.sub("([A-Za-z]*://)?(www*.)?(\\.[A-Za-z]*)?(/[A-Za-z\\?]*)?", "", url)
     #url = re.sub("[A-Za-z]*://", "", url)
     #url = re.sub("www*.", "", url)
     #url = re.sub("\.[A-Za-z]*", "", url)
@@ -16,5 +16,5 @@ def test_domain_name():
     assert domain_name("www.google.com") == "google"
     assert domain_name("http://www.google.com/test.txt") == "google"
     assert domain_name("http://www.google.com/testen/") == "google"
-    # FixMe: 
-    # assert domain_name("http://www.youtube.com/watch?dsfdrjkdf") == "youtube"
+
+    assert domain_name("http://www.youtube.com/watch?dsfdrjkdf") == "youtube"
