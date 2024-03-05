9a4cf6bee38eb7de9fee7a586fc82de92b7f4fd9
djkeu <mrkooij@gmail.com>
2024-03-05 18:52:10 +0100

    Add tests, add FixMe, remove domain_from_url_test.py

--- domain_from_url.py
+++ domain_from_url.py
@@ -14,3 +14,7 @@ def test_domain_name():
     assert domain_name("http://www.google.com") == "google"
     assert domain_name("http://google.com") == "google"
     assert domain_name("www.google.com") == "google"
+    assert domain_name("http://www.google.com/test.txt") == "google"
+    assert domain_name("http://www.google.com/testen/") == "google"
+    # FixMe: 
+    # assert domain_name("http://www.youtube.com/watch?dsfdrjkdf") == "youtube"
--- domain_from_url_test.py
+++
@@ -1,7 +0,0 @@
-from domain_from_url import domain_name
-
-
-def test_domain_name():
-    assert domain_name("http://www.google.com") == "google"
-    assert domain_name("http://google.com") == "google"
-    assert domain_name("www.google.com") == "google"
