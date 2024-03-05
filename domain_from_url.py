2db0ca3ed2cb48bcbe3c216dcab898d66d7f9f24
djkeu <mrkooij@gmail.com>
2024-03-05 18:56:42 +0100

    Move obsolete code below

--- domain_from_url.py
+++ domain_from_url.py
@@ -3,10 +3,6 @@ import re
 
 def domain_name(url):
     url = re.sub("([A-Za-z]*://)?(www*.)?(\\.[A-Za-z]*)?(/[A-Za-z\\?]*)?", "", url)
-    #url = re.sub("[A-Za-z]*://", "", url)
-    #url = re.sub("www*.", "", url)
-    #url = re.sub("\.[A-Za-z]*", "", url)
-    #url = re.sub("/[A-Za-z]*", "", url)
 
     return url
 
@@ -19,3 +15,9 @@ def test_domain_name():
     assert domain_name("http://www.google.com/testen/") == "google"
 
     assert domain_name("http://www.youtube.com/watch?dsfdrjkdf") == "youtube"
+
+
+    #url = re.sub("[A-Za-z]*://", "", url)
+    #url = re.sub("www*.", "", url)
+    #url = re.sub("\.[A-Za-z]*", "", url)
+    #url = re.sub("/[A-Za-z]*", "", url)
