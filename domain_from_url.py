324a143fd87fca62f5666221f04be5697ca53ba5
djkeu <mrkooij@gmail.com>
2024-03-05 18:27:13 +0100

    Add pytests

--- domain_from_url.py
+++ domain_from_url.py
@@ -1,14 +1,16 @@
 import re
 
 def domain_name(url):
-    url = re.sub("([A-Za-z]*://)?(www*.)?(\.[A-Za-z]*)?(/[A-Za-z]*)?", "", url)
+    url = re.sub("([A-Za-z]*://)?(www*.)?(\\.[A-Za-z]*)?(/[A-Za-z]*)?", "", url)
     #url = re.sub("[A-Za-z]*://", "", url)
     #url = re.sub("www*.", "", url)
     #url = re.sub("\.[A-Za-z]*", "", url)
     #url = re.sub("/[A-Za-z]*", "", url)
 
-    print(url)
+    return url
 
-domain_name("http://www.google.com")
-domain_name("www.google.com")
-domain_name("http://google.com")
+
+def test_domain_name():
+    assert domain_name("http://www.google.com") == "google"
+    assert domain_name("http://google.com") == "google"
+    assert domain_name("www.google.com") == "google"
