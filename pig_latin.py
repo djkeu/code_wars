02f37d1f51454a29cd4bdac84e79d6ecb11ea11a
djkeu <mrkooij@gmail.com>
2024-03-06 14:54:45 +0100

    Complete pig_it()

--- pig_latin.py
+++ pig_latin.py
@@ -1,18 +1,16 @@
 def pig_it(text):
-    """
-    Move the first letter of each word to the end of it, then add "ay" to the end of the word. Leave punctuation marks untouched.
-    """
     words = text.split()
-    print(words)
+    new_words = ""
+
     for word in words:
-        print(word)
+        if word.isalpha():
+            word = word[1:] + word[0] + "ay"
+        new_words += word + " "
 
-
-pig_it("This is my string !")
+    return new_words.strip()
 
 
 def test_pig_it():
     assert pig_it("Pig latin is cool") == "igPay atinlay siay oolcay"
     assert pig_it("Hello world !") == "elloHay orldway !"
     assert pig_it("This is my string") == "hisTay siay ymay tringsay"
-
