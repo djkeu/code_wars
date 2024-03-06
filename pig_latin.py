d243684edc1cd87616f5ba53544d9dcdf31b3025
djkeu <mrkooij@gmail.com>
2024-03-06 09:41:17 +0100

    Create test_pig_it(), move instructions to docstring

--- pig_latin.py
+++ pig_latin.py
@@ -1,21 +1,12 @@
-
-"""
-Move the first letter of each word to the end of it, then add "ay" to the end of the word. Leave punctuation marks untouched.
-
-Examples:
-pig_it('Pig latin is cool') # igPay atinlay siay oolcay
-pig_it('Hello world !')     # elloHay orldway !
-"""
-"""
-test.assert_equals(pig_it('Pig latin is cool'),'igPay atinlay siay oolcay')
-test.assert_equals(pig_it('This is my string'),'hisTay siay ymay tringsay')
-"""
-
-"""
-regular expressions, algorithms
-"""
 def pig_it(text):
-    pass
+    """
+    Move the first letter of each word to the end of it, then add "ay" to the end of the word. Leave punctuation marks untouched.
+    """
+    # code
 
 
+def test_pig_it():
+    assert pig_it("Pig latin is cool") == "igPay atinlay siay oolcay"
+    assert pig_it("Hello world !") == "elloHay orldway !"
+    assert pig_it("This is my string") == "hisTay siay ymay tringsay"
 
