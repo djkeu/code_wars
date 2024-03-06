54da2a0b6960323954642e67212ac363c20ffa41
djkeu <mrkooij@gmail.com>
2024-03-06 09:55:30 +0100

    Print words

--- pig_latin.py
+++ pig_latin.py
@@ -3,6 +3,7 @@ def pig_it(text):
     Move the first letter of each word to the end of it, then add "ay" to the end of the word. Leave punctuation marks untouched.
     """
     words = text.split()
+    print(words)
     for word in words:
         print(word)
 
