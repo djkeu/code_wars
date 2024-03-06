8e5b12c77f5560dbb1efc9a9d46633ae5f301397
djkeu <mrkooij@gmail.com>
2024-03-06 01:39:08 +0100

    Start Simple pig latin

--- README.md
+++ README.md
@@ -4,3 +4,6 @@ www.codewars.com
 ### exercises
 - Extract the domain name from a URL\
 domain_from_url.py
+- Simple Pig Latin\
+pig_latin.py
+
---
+++ pig_latin.py
@@ -0,0 +1,21 @@
+
+"""
+Move the first letter of each word to the end of it, then add "ay" to the end of the word. Leave punctuation marks untouched.
+
+Examples:
+pig_it('Pig latin is cool') # igPay atinlay siay oolcay
+pig_it('Hello world !')     # elloHay orldway !
+"""
+"""
+test.assert_equals(pig_it('Pig latin is cool'),'igPay atinlay siay oolcay')
+test.assert_equals(pig_it('This is my string'),'hisTay siay ymay tringsay')
+"""
+
+"""
+regular expressions, algorithms
+"""
+def pig_it(text):
+    pass
+
+
+
