3017822c319bed8a988bf6e57b5dba477f532d1a
djkeu <mrkooij@gmail.com>
2024-03-10 15:06:50 +0100

    Complete is_it_letter.py

---
+++ is_it_letter.py
@@ -0,0 +1,17 @@
+# https://www.codewars.com/kata/57a06b07cf1fa58b2b000252/train/python
+
+
+def is_it_letter(s):
+    if s.isalpha():
+        return True
+    else:
+        return False
+
+def test_is_it_letter():
+    assert is_it_letter('a') == True
+    assert is_it_letter('A') == True
+    assert is_it_letter('!') == False
+    assert is_it_letter('1') == False
+
+
+is_it_letter("a")
