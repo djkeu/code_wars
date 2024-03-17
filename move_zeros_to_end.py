b29e4d055f7bf1d2211517f6e016b71722504e42
djkeu <mrkooij@gmail.com>
2024-03-17 21:44:01 +0100

    Pytest

--- move_zeros_to_end.py
+++ move_zeros_to_end.py
@@ -3,13 +3,11 @@
 
 
 def move_zeros(lst):
-    print(f"Original list: {lst}")
     for n in lst:
         if n == 0:
             lst.append(lst.pop(lst.index(n)))
-
-    print(f"Altered list: {lst}")
     return lst
 
-move_zeros([1, 0, 1, 2, 0, 1, 3])
-print(f"Expected list: [1, 1, 2, 1, 3, 0, 0]")
+
+def test_move_zeros():
+    assert move_zeros([1, 0, 1, 2, 0, 1, 3]) == [1, 1, 2, 1, 3, 0, 0]
