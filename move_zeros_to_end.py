9160aacacb1724b5decbc49a1439f6b157533742
djkeu <mrkooij@gmail.com>
2024-03-17 21:46:18 +0100

    Add tests from Codewars

--- move_zeros_to_end.py
+++ move_zeros_to_end.py
@@ -11,3 +11,7 @@ def move_zeros(lst):
 
 def test_move_zeros():
     assert move_zeros([1, 0, 1, 2, 0, 1, 3]) == [1, 1, 2, 1, 3, 0, 0]
+    assert move_zeros([9, 0, 0, 9, 1, 2, 0, 1, 0, 1, 0, 3, 0, 1, 9, 0, 0, 0, 0, 9]) == [9, 9, 1, 2, 1, 1, 3, 1, 9, 9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    assert move_zeros([0, 0]) == [0, 0]
+    assert move_zeros([0]) == [0]
+    assert move_zeros([]) == []
