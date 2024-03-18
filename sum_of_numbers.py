c49f5f7fc1350edec9139d623cfaf0e1f1448651
djkeu <mrkooij@gmail.com>
2024-03-18 13:33:37 +0100

    Add tests, improve readability test_get_sum()

--- sum_of_numbers.py
+++ sum_of_numbers.py
@@ -12,15 +12,26 @@ def get_sum(a,b):
     return sum
 
 
-def test_get_sum():
-    assert get_sum(0, 1) == 1
-    assert get_sum(0,-1) == -1
-    assert get_sum(1, 0) == 1  # (1 + 0 = 1)
-    assert get_sum(1, 2) == 3  # (1 + 2 = 3)
-    assert get_sum(0, 1) == 1  # (0 + 1 = 1)
-    assert get_sum(1, 1) == 1  # (1 since both are same)
-    assert get_sum(-1, 0) == -1  # (-1 + 0 = -1)
-    assert get_sum(-1, 2) == 2  # (-1 + 0 + 1 + 2 = 2)
+def test_get_sum_a_equals_b():
+    assert get_sum(1, 1) == 1
+    assert get_sum(12, 12) == 12
+    assert get_sum(-1, -1) == -1
+    assert get_sum(-12, -12) == -12
+    assert get_sum(0, 0) == 0
+
+
+def test_get_sum_b_larger_than_a():
+    assert get_sum(0, 1) == 1       # (0 = 1 = 1)
+    assert get_sum(1, 3) == 6       # (1 + 2 + 3 = 6)
+    assert get_sum(-1, 0) == -1     # (-1 + 0 = -1)
+    assert get_sum(-1, 2) == 2      # (-1 + 0 + 1 + 2 = 2)
+
+
+def test_get_sum_a_larger_than_b():
+    assert get_sum(1, 0) == 1       # (1 + 0 = 1)
+    assert get_sum(2, 1) == 3       # (0 + -1 = -1)
+    assert get_sum(0,-1) == -1      # (0 + -1 = -1)
+    assert get_sum(2, -1) == 2      # (1 + 0 = 1)
 
 
 # https://www.codewars.com/kata/55f2b110f61eb01779000053/train/python
