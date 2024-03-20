06aae4bd2155370aa446964ef1e2a4e9783fc2ee
djkeu <mrkooij@gmail.com>
2024-03-20 12:21:43 +0100

    Create test_square_sum()

--- square_sum.py
+++ square_sum.py
@@ -1,7 +1,14 @@
 def square_sum(numbers):
     #your code here
+    ...
 
 
+def test_square_sum():
+    assert square_sum([1,2]) == 5
+    assert square_sum([0, 3, 4, 5]) == 50
+    assert square_sum([]) == 0
+    assert square_sum([-1,-2]) == 5
+    assert square_sum([-1,0,1]) == 2
 
 
 # https://www.codewars.com/kata/515e271a311df0350d00000f/train/python
