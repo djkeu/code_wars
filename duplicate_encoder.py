2200f3680705817b815b3259cd5e71171d5ac35a
djkeu <mrkooij@gmail.com>
2024-03-22 14:49:55 +0100

    Add tests

--- duplicate_encoder.py
+++ duplicate_encoder.py
@@ -2,6 +2,13 @@ def duplicate_encode(word):
     ...
 
 
+def test_duplicate_encode():
+    assert duplicate_encode("din") == "((("
+    assert duplicate_encode("recede") == "()()()"
+    assert duplicate_encode("Success") == ")())())"  #"should ignore case"
+    assert duplicate_encode("(( @") == "))(("
+
+
 """
 Instructions:
 The goal of this exercise is to convert a string to a new string where each character in the new string is "(" if that character appears only once in the original string, or ")" if that character appears more than once in the original string. Ignore capitalization when determining if a character is a duplicate.
