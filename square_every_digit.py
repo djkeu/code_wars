6ab9e02c9ff2653cfb51ebc0919a623e41a749c4
djkeu <mrkooij@gmail.com>
2024-03-22 08:48:43 +0100

    Complete square_digits()

--- square_every_digit.py
+++ square_every_digit.py
@@ -1,6 +1,18 @@
 def square_digits(num):
-    ...
-    
+    squared_numbers = []
+
+    for digit in map(int, str(num)):
+        squared_numbers.append(digit**2)
+    print(squared_numbers)
+
+    concatenated_number = ""
+    for number in squared_numbers:
+        concatenated_number += str(number)
+    print(concatenated_number)
+
+square_digits(9119)
+
+
 
 
 # https://www.codewars.com/kata/546e2562b03326a88e000020/train/python
