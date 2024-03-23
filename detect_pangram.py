e33e07ce8f0826bc584786daf3ab14545295b157
djkeu <mrkooij@gmail.com>
2024-03-23 12:12:16 +0100

    Keep testing

--- detect_pangram.py
+++ detect_pangram.py
@@ -3,12 +3,18 @@ import string
 
 def is_pangram(s):
     list_alphabet = list(string.ascii_lowercase)
+    list_string = list(s.lower())
+    #print(list_alphabet)
+    #print(list_string)
 
     for ch in list_alphabet:
-        if ch not in list(s):
+        if ch not in list_string:
             return False
         else:
             return True
+
+is_pangram("Hallo")
+is_pangram("bcdefghijklmnopqrstuvwxyz")
 
 
 def test_pangram():        
@@ -17,3 +23,4 @@ def test_pangram():
 
 def test_not_pangram():        
     assert is_pangram("1bcdefghijklmnopqrstuvwxyz") == False
+    assert is_pangram("Aacdefghijklmnopqrstuvwxyz") == False
