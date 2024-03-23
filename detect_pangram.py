667c3baeebff6680200cd86f02a6527a1ff2f9d0
djkeu <mrkooij@gmail.com>
2024-03-23 11:34:25 +0100

    Copy tests from CW, adjust to pytest

--- detect_pangram.py
+++ detect_pangram.py
@@ -2,3 +2,9 @@ def is_pangram(s):
     return False
 
 
+def test_pangram():        
+    assert is_pangram("The quick, brown fox jumps over the lazy dog!") == True
+
+
+def test_not_pangram():        
+    assert is_pangram("1bcdefghijklmnopqrstuvwxyz") == False
