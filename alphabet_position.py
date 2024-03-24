b2a4aec973592cd97ad95fe4d19f81229c281a1f
djkeu <mrkooij@gmail.com>
2024-03-24 13:58:52 +0100

    Remove abundant function call

--- alphabet_position.py
+++ alphabet_position.py
@@ -16,8 +16,6 @@ def alphabet_position(text):
 
     return numbered_text.strip()
 
-print(alphabet_position("test"))
-
 
 def test_alphabet_position():
     assert alphabet_position("The sunset sets at twelve o' clock.") == "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11"
