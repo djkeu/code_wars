4deafae2391a09f179c9b5e194d98fb51e8b41f8
djkeu <mrkooij@gmail.com>
2024-03-24 17:11:23 +0100

    Add failed CW tests to test_vowel_indices()

--- find_vowels.py
+++ find_vowels.py
@@ -6,6 +6,7 @@ def vowel_indices(word):
         if ch in list("aeiouy"):
             found_vowels.append(word.index(ch) + 1)
 
+    found_vowels = set(found_vowels)
     return found_vowels
     
 
@@ -14,9 +15,12 @@ def test_vowel_indices():
     assert vowel_indices("apple") == [1,5]
     assert vowel_indices("123456") == []
     assert vowel_indices("UNDISARMED") == [1,4,6,9]
-
+    assert vowel_indices("supercalifragilisticexpialidocious") == [2, 4, 7, 9, 12, 14, 16, 19, 21, 24, 25, 27, 29, 31, 32, 33]
+    assert vowel_indices("Implied") == [1, 5, 6]
+    
 
 """
+Instructions:
 We want to know the index of the vowels in a given word, for example, there are two vowels in the word super (the second and fourth letters).
 
 So given a string "super", we should return a list of [2, 4].
