628aa933dad76e4fcdcbda0e1f41362b9fa5f520
djkeu <mrkooij@gmail.com>
2024-03-24 12:52:51 +0100

    Add test_jaden_case()

--- jaden_casing_strings.py
+++ jaden_casing_strings.py
@@ -2,4 +2,9 @@ def to_jaden_case(string):
     ...
 
 
+def test_jaden_case():
+    quote = "How can mirrors be real if our eyes aren't real"
+    assert to_jaden_case(quote) ==  "How Can Mirrors Be Real If Our Eyes Aren't Real"
+
+
 # https://www.codewars.com/kata/5390bac347d09b7da40006f6/train/python
