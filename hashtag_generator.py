715f86c84df2ba9be60b09fef0b4de2cf6e70b5a
djkeu <mrkooij@gmail.com>
2024-03-25 09:24:11 +0100

    Add tests and adjust to pytest

--- hashtag_generator.py
+++ hashtag_generator.py
@@ -3,6 +3,20 @@ def generate_hashtag(s):
     pass
 
 
+def test_correct_hashtag_fixed():
+    assert generate_hashtag('Codewars') == '#Codewars'
+    assert generate_hashtag('Codewars      ') == '#Codewars'
+    assert generate_hashtag('      Codewars') == '#Codewars'
+    assert generate_hashtag('Codewars Is Nice') == '#CodewarsIsNice'
+    assert generate_hashtag('codewars is nice') == '#CodewarsIsNice'
+    assert generate_hashtag('CoDeWaRs is niCe') == '#CodewarsIsNice'
+    assert generate_hashtag('c i n') == '#CIN'
+    assert generate_hashtag('codewars  is  nice') == '#CodewarsIsNice'
+    
+# "Should return False if the input is empty, or result is longer than 140 chars"
+def test_false_hashtag_fixed():
+    assert generate_hashtag('') == False
+    assert generate_hashtag('Looooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooong Cat') == False
 
 
 """
