fb3f6717b9f9b9c7f42b657b025882c5b5403d0b
djkeu <mrkooij@gmail.com>
2024-03-26 08:44:56 +0100

    Add tests

--- exclamation_vowels.py
+++ exclamation_vowels.py
@@ -2,6 +2,11 @@ def replace_exclamation(st):
     return st
 
 
+def test_replace_exclamation():
+    assert replace_exclamation("Hi!") == "H!!"
+    assert replace_exclamation("!Hi! Hi!") == "!H!! H!!"
+    assert replace_exclamation("aeiou") == "!!!!!"
+    assert replace_exclamation("ABCDE") == "!BCD!"
 
 
 # https://www.codewars.com/kata/57fb09ef2b5314a8a90001ed/train/python
